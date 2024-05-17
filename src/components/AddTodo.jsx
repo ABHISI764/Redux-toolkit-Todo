93cff60d2fef6913ddaa58e7e609b872895e6994
Abhishek Pratap Singh <abhisi764@gmail.com>
2024-05-18 00:44:13 +0530

    add update todo features

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -8,22 +8,24 @@ function AddTodo() {
     const [input, setInput] = useState('')
     const dispatch = useDispatch()
 
+
     const addTodoHandler = (e) => {
         e.preventDefault()
         dispatch(addTodo(input))
         setInput('')
     }
-
-
   return (
     <div>
-        <form onSubmit={addTodoHandler} style={{ display: 'flex', marginBottom: '10px' }}>
+        <form onSubmit={addTodoHandler} 
+        style={{ display: 'flex', marginBottom: '10px' }}>
+
             <input
             type='text'
             placeholder='Enter a todo...'
             value={input}
             onChange={(e) => setInput(e.target.value)}
             />
+            
             <button 
             style={{ padding: '5px', marginLeft: '5px', backgroundColor: 'blue', color: 'white', border: 'none', cursor: 'pointer' }}
             onClick={addTodoHandler}>Add Todo</button>
