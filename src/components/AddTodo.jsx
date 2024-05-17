953836b24841359b8d042b4f04a64b6439205fe4
Abhishek Pratap Singh <abhisi764@gmail.com>
2024-05-17 23:56:59 +0530

    complete git todo

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../features/todo/todoSlice' 
+
+
+
+function AddTodo() {
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+
+  return (
+    <div>
+        <form onSubmit={addTodoHandler} style={{ display: 'flex', marginBottom: '10px' }}>
+            <input
+            type='text'
+            placeholder='Enter a todo...'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            />
+            <button 
+            style={{ padding: '5px', marginLeft: '5px', backgroundColor: 'blue', color: 'white', border: 'none', cursor: 'pointer' }}
+            onClick={addTodoHandler}>Add Todo</button>
+
+        </form>
+    </div>
+  )
+}
+
+export default AddTodo
---
+++ src/components/Todos.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+function Todos() {
+  const todos =  useSelector(state => state.todos)
+  const dispatch = useDispatch()
+  return (
+    <>
+     <div style={{ marginBottom: '10px' }}>Todos</div>
+     {
+        todos.map((todo)=> (
+            <li 
+            style={{ marginBottom: '5px', backgroundColor: '#f4f4f4', padding: '10px', borderRadius: '5px', display: 'flex', justifyContent: 'space-between' }}
+            key={todo.id}>
+                {todo.text} 
+                <button
+                 style={{ backgroundColor: 'red', color: 'white', border: 'none', borderRadius: '3px', padding: '5px', cursor: 'pointer' }}
+                onClick={()=> dispatch(removeTodo(todo.id))}
+                >X</button>
+            </li>
+        ))
+     }
+    </>
+   
+  )
+}
+
+export default Todos
